8b096c2bb0d5ddc61e4bbf196d3dece738c3cd87
Kian Pouresmaeil <icdef007@gmail.com>
2024-12-02 20:01:05 +0100

    done day2

--- src/day2/Main.java
+++ src/day2/Main.java
@@ -2,6 +2,7 @@ package day2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -17,14 +18,23 @@ public class Main {
             String line = scanner.nextLine();
             String[] split = line.split(" ");
             List<Integer> numbers = new java.util.ArrayList<>(Arrays.stream(split).map(Integer::parseInt).toList());
-            if (numbers.get(0).equals(numbers.get(1)))
-                continue;
-            isIncreasing = numbers.get(0) < numbers.get(1);
-
-            if (isIncreasing && isSafeIncreasing(numbers) == -1) {
+            int indexIncrease = isSafeIncreasing(numbers);
+            int indexDecrease = isSafeDecreasing(numbers);
+            if (indexIncrease == -1 || indexDecrease == -1) {
                 sum++;
-            } else if (!isIncreasing && isSafeDecreasing(numbers) == -1) {
-                sum++;
+            } else {
+                List<Integer> retryIncrease = new ArrayList<>(numbers);
+                retryIncrease.remove(indexIncrease);
+                List<Integer> retryIncreasePlus = new ArrayList<>(numbers);
+                retryIncreasePlus.remove(indexIncrease+1);
+                List<Integer> retryDecrease = new ArrayList<>(numbers);
+                retryDecrease.remove(indexDecrease);
+                List<Integer> retryDecreasePlus = new ArrayList<>(numbers);
+                retryDecreasePlus.remove(indexDecrease+1);
+                if (isSafeIncreasing(retryIncrease) == -1 || isSafeIncreasing(retryIncreasePlus) == -1
+                || isSafeDecreasing(retryDecrease) == -1 || isSafeDecreasing(retryDecreasePlus) == -1) {
+                    sum ++;
+                    }
             }
 
         }
