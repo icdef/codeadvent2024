4f509bffc7c498cb3d3c0ad886b3677de13e86b4
Kian Pouresmaeil <icdef007@gmail.com>
2024-12-08 21:33:03 +0100

    day8 part2 done

--- src/day8/Main.java
+++ src/day8/Main.java
@@ -21,9 +21,12 @@ public class Main {
                     map.computeIfAbsent(c, k -> new ArrayList<>());
                     List<Coord> coords = map.get(c);
                     coords.add(new Coord(j, i));
-
                 }
             }
+        }
+        Set<Coord> allAntennaCoords = new HashSet<>();
+        for (Map.Entry<Character, List<Coord>> entry : map.entrySet()) {
+            allAntennaCoords.addAll(entry.getValue());
         }
         Set<Coord> antiNodes = new HashSet<>();
         for (Map.Entry<Character, List<Coord>> entry : map.entrySet()) {
@@ -36,13 +39,21 @@ public class Main {
                     int yDiff = left.y - right.y;
                     int x1 = right.x - xDiff;
                     int y1 = right.y - yDiff;
-                    if (x1 >= 0 && x1 < field.get(i).length && y1 >= 0 && y1 < field.size()) {
-                        antiNodes.add(new Coord(x1, y1));
+                    while (x1 >= 0 && x1 < field.get(i).length && y1 >= 0 && y1 < field.size()) {
+                        Coord e = new Coord(x1, y1);
+                        if (!allAntennaCoords.contains(e))
+                            antiNodes.add(e);
+                        x1 = x1 - xDiff;
+                        y1 = y1 - yDiff;
                     }
                     int x2 = left.x + xDiff;
                     int y2 = left.y + yDiff;
-                    if (x2 >= 0 && x2 < field.get(i).length && y2 >= 0 && y2 < field.size()) {
-                        antiNodes.add(new Coord(x2, y2));
+                    while (x2 >= 0 && x2 < field.get(i).length && y2 >= 0 && y2 < field.size()) {
+                        Coord e = new Coord(x2, y2);
+                        if (!allAntennaCoords.contains(e))
+                            antiNodes.add(e);
+                        x2 = x2 + xDiff;
+                        y2 = y2 + yDiff;
                     }
 
                 }
@@ -54,12 +65,11 @@ public class Main {
                 field.get(coord.y)[coord.x] = '#';
         }
         drawField(field);
-
+        int count = 0;
         for (Map.Entry<Character, List<Coord>> entry : map.entrySet()) {
-            System.out.println(entry.getKey());
-            entry.getValue().forEach(System.out::println);
+            count += entry.getValue().size();
         }
-        System.out.println(antiNodes.size());
+        System.out.println(antiNodes.size() + count);
 
     }
 
